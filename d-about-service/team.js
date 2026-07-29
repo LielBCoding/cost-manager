24dc6c9cb727048b3d7edd9a9576567a1076bd79
LielBCoding <LielBCoding@users.noreply.github.com>
2026-07-29 23:19:47 +0300

    Fix last name spelling

--- d-about-service/team.js
+++ d-about-service/team.js
@@ -1,6 +1,6 @@
 // the two students who built the project. these names are not kept in the db.
 const TEAM = [
-  { first_name: 'Liel', last_name: 'Babayan' },
+  { first_name: 'Liel', last_name: 'Babayn' },
   { first_name: 'Matan', last_name: 'Dalal' },
 ];
 
